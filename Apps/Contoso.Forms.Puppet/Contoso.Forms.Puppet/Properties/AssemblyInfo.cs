3f01bf8d1474ae5638938eb96ec60c7e7e4a0fea
nor0x <nor0x@users.noreply.github.com>
2021-06-22 08:43:18 +0200

    version bump to 4.3.0

--- Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet/Properties/AssemblyInfo.cs
+++ Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("4.1.0.0")]
-[assembly: AssemblyInformationalVersion("4.2.0-XMAC")]
+[assembly: AssemblyInformationalVersion("4.3.0-XMAC")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
--- Tests/Microsoft.AppCenter.Crashes.Test.Windows/Properties/AssemblyInfo.cs
+++ Tests/Microsoft.AppCenter.Crashes.Test.Windows/Properties/AssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("4.1.0.0")]
-[assembly: AssemblyInformationalVersion("4.2.0-XMAC")]
+[assembly: AssemblyInformationalVersion("4.3.0-XMAC")]
